7315c573007e2181eafa995838942adccda47eb9
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-10-24 16:31:47 -0700

    Add massive return from collection.

--- src/test/java/FilteringReadTest.java
+++ src/test/java/FilteringReadTest.java
@@ -4,11 +4,15 @@ import org.junit.Test;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.net.UnknownHostException;
+import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 public class FilteringReadTest {
+    private static final int LIMIT = 100;
+
     private MongoRunner mongoRunner;
 
     @Before
@@ -16,12 +20,11 @@ public class FilteringReadTest {
 
     @Test public void shouldSuccessfullyGetCC() throws UnknownHostException {
         Query query = new Query();
-        query.addCriteria(where("ccNumber").is("851470006"));
+        query.addCriteria(where("brandId").is("1"));
         query.fields().include("ccNumber");
+        query.limit(LIMIT);
 
-        for(int i = 0; i < 100; i++) {
-            AllocationCC cc = mongoRunner.getTemplate().findOne(query, AllocationCC.class);
-            assertNotNull(cc);
-        }
+        List<AllocationCC> ccs = mongoRunner.getTemplate().find(query, AllocationCC.class);
+        assertThat(ccs.size(), is(LIMIT));
     }
 }
